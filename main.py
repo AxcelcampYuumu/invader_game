761e8702ccb38a68013903adc22f76a5f117e47e
AxcelcampYuumu <axcelcamp.yuumu@gmail.com>
2021-09-30 19:31:21 +0900

    ９月２９日

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+"""
+やってる処理ごとに見やすくなるように、うまく改行をいれてみよう
+例）
+画像関連の処理１
+画像関連の処理２
+
+色の設定関連１
+色の設定関連２
+
+
+
+"""
+
 import pygame
 from  pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 screen.fill((0,0,0))
+player = pygame.image.load("player.png")
+playerX = 200
+playerY = 100
 pygame.display.set_caption("インベーダー")
 
 
@@ -11,6 +27,7 @@ running = True
 
 
 while running:
+    screen.blit(player, (playerX, playerY))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -26,7 +43,6 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_DOWN:
                 screen.fill((0, 0, 0))
-
 
 
 
